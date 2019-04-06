636f916ca65c1f822375f4335ebf4297a4b0b061
Joshua Fernandes <joshferns0212@gmail.com>
2019-04-06 00:19:47 -0400

    Formatting

--- Assignment 10/Assignment10_Q1.cpp
+++ Assignment 10/Assignment10_Q1.cpp
@@ -16,6 +16,7 @@ class Task
 		//data members
 		string taskName;
 		int startTime, duration;
+
 	public:
 		//constructors
 		Task()
@@ -26,10 +27,10 @@ class Task
 		}
 		Task(string inputTaskName, int inputStartTime, int inputDuration)
 		{
-			Task ();
-			setTaskName (inputTaskName);
-			setStartTime (inputStartTime);
-			setDuration (inputDuration);
+			Task();
+			setTaskName(inputTaskName);
+			setStartTime(inputStartTime);
+			setDuration(inputDuration);
 		}
 		//accessor methods
 		string getTaskName() const
@@ -51,8 +52,7 @@ class Task
 		}
 		void setStartTime(int inputStartTime)
 		{
-			if (inputStartTime >= 800 && inputStartTime <= 1600
-				&& inputStartTime == inputStartTime / 100 * 100)
+			if (inputStartTime >= 800 && inputStartTime <= 1600 && inputStartTime == inputStartTime / 100 * 100)
 				startTime = inputStartTime;
 		}
 		void setDuration(int inputDuration)
@@ -69,26 +69,28 @@ class Task
 		{
 			return endsAt() > 1700;
 		}
-		/*Tasks conflict when the ending of one task occurs after the beginning
-		of the next task. This means the ending time of the first task is
-		greater than the start time of the next task*/
-		bool overlaps(Task const & nextTask) const
+		/**
+		 * Tasks conflict when the ending of one task occurs after the beginning
+		 * of the next task. This means the ending time of the first task is
+		 * greater than the start time of the next task
+		**/
+		bool overlaps(Task const &nextTask) const
 		{
 			return endsAt() > nextTask.startTime;
 		}
-		void operator= (Task const & task)
+		void operator=(Task const &task)
 		{
 			taskName = task.getTaskName();
 			startTime = task.getStartTime();
 			duration = task.getDuration();
 		}
 		//friend functions
-		friend ostream& operator<< (ostream & out, Task const & task);
-		friend ostream& operator<< (ostream & out, Task tasks[MAX_NUM_OF_TASKS]);
-		friend istream& operator>> (istream & in, Task const & task);
-		friend istream& operator>> (istream & in, Task tasks[MAX_NUM_OF_TASKS]);
+		friend ostream &operator<<(ostream &out, Task const &task);
+		friend ostream &operator<<(ostream &out, Task tasks[MAX_NUM_OF_TASKS]);
+		friend istream &operator>>(istream &in, Task const &task);
+		friend istream &operator>>(istream &in, Task tasks[MAX_NUM_OF_TASKS]);
 };
-void swap (Task & task1, Task & task2)
+void swap(Task &task1, Task &task2)
 {
 	Task tempTask;
 
@@ -97,29 +99,29 @@ void swap (Task & task1, Task & task2)
 	task2 = tempTask;
 }
 //overloaded operators
-ostream& operator<<(ostream & out, Task const & task)
+ostream &operator<<(ostream &out, Task const &task)
 {
 	out << task.taskName << ": " << task.startTime << " " << task.duration;
 	return out;
 }
-ostream& operator<<(ostream & out, Task tasks [MAX_NUM_OF_TASKS])
+ostream &operator<<(ostream &out, Task tasks[MAX_NUM_OF_TASKS])
 {
 	int numOfConflicts = 0;
 	for (int i = 0; i < actualNumOfTasks; i++)
 	{
-			out << tasks[i];
+		out << tasks[i];
 
-			if (tasks[i].isTooLong())
-				cout << " late";
-			if (i > 0 && tasks[i - 1].overlaps(tasks[i]))
-				numOfConflicts++;
+		if (tasks[i].isTooLong())
+			cout << " late";
+		if (i > 0 && tasks[i - 1].overlaps(tasks[i]))
+			numOfConflicts++;
 
-			cout << endl;
+		cout << endl;
 	}
 
 	cout << "Number of conflicts: " << numOfConflicts;
 }
-istream& operator>>(istream & in, Task & task)
+istream &operator>>(istream &in, Task &task)
 {
 	string tempTaskName = "";
 	int tempStartTime = 0, tempDuration = 0;
@@ -132,30 +134,30 @@ istream& operator>>(istream & in, Task & task)
 
 	return in;
 }
-istream& operator>>(istream & in, Task tasks[MAX_NUM_OF_TASKS])
+istream &operator>>(istream &in, Task tasks[MAX_NUM_OF_TASKS])
 {
 	for (int i = 0; i < MAX_NUM_OF_TASKS; i++)
 	{
-		in>>tasks[i];
+		in >> tasks[i];
 		if (tasks[i].getTaskName() == "")
 			break;
 		actualNumOfTasks++;
 	}
 
-	 int minIndex;
+	int minIndex;
 
-	 for (int i = 0; i < actualNumOfTasks - 1; i++)
-	 {
-	 	minIndex = i;
+	for (int i = 0; i < actualNumOfTasks - 1; i++)
+	{
+		minIndex = i;
 
-	 	for (int j = i + 1; j < actualNumOfTasks; j++)
-	 	{
-	 		if (tasks[j].getStartTime() < tasks[minIndex].getStartTime())
-	 			minIndex = j;
-	 	}
+		for (int j = i + 1; j < actualNumOfTasks; j++)
+		{
+			if (tasks[j].getStartTime() < tasks[minIndex].getStartTime())
+				minIndex = j;
+		}
 
-	 	swap(tasks[minIndex], tasks[i]);
-	 }
+		swap(tasks[minIndex], tasks[i]);
+	}
 }
 int main()
 {
