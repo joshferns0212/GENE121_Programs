51ba566dda17e2854182b14bf34e980243f15ed5
Joshua Fernandes <joshferns0212@gmail.com>
2019-04-19 18:55:17 -0400

    Removed spaces

--- Assignment1_Q3.cpp
+++ Assignment1_Q3.cpp
@@ -4,18 +4,18 @@
 
 using namespace std;
 
-int main ()
+int main()
 {
 	float numFloat = 0;
 	double numDouble = 0;
 	long double numLongDouble = 0;
-	
+
 	cout << "Enter angle in degrees.\n"; //float
 	cin >> numFloat;
-	
+
 	numFloat *= M_PI / 180.0;
-	
-	if ((pow (sin (numFloat),2) + pow (cos (numFloat), 2) - 1) == 0)
+
+	if ((pow(sin(numFloat), 2) + pow(cos(numFloat), 2) - 1) == 0)
 	{
 		cout << "d = sin^2x + cos^2x - 1 is equal to zero.\n";
 	}
@@ -23,13 +23,13 @@ int main ()
 	{
 		cout << "d = sin^2x + cos^2x - 1 is not equal to zero.\n";
 	}
-	
+
 	cout << "Enter angle in degrees.\n"; //double
 	cin >> numDouble;
-	
+
 	numDouble *= M_PI / 180.0;
-	
-	if ((pow (sin (numDouble),2) + pow (cos (numDouble), 2) - 1) == 0)
+
+	if ((pow(sin(numDouble), 2) + pow(cos(numDouble), 2) - 1) == 0)
 	{
 		cout << "d = sin^2x + cos^2x - 1 is equal to zero.\n";
 	}
@@ -37,13 +37,13 @@ int main ()
 	{
 		cout << "d = sin^2x + cos^2x - 1 is not equal to zero.\n";
 	}
-	
+
 	cout << "Enter angle in degrees.\n"; //long double
 	cin >> numLongDouble;
-	
+
 	numDouble *= M_PI / 180.0;
-	
-	if ((pow (sin (numDouble),2) + pow (cos (numDouble), 2) - 1) == 0)
+
+	if ((pow(sin(numDouble), 2) + pow(cos(numDouble), 2) - 1) == 0)
 	{
 		cout << "d = sin^2x + cos^2x - 1 is equal to zero.\n";
 	}
@@ -51,5 +51,5 @@ int main ()
 	{
 		cout << "d = sin^2x + cos^2x - 1 is not equal to zero.\n";
 	}
-	
+	return EXIT_SUCCESS;
 }
